a70756849a3c353a2db62fa86b5e55adcbfcd0f8
NETyENeTA <gigasekond2@gmail.com>
2025-03-30 10:34:06 +0500

    ффф

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-import {fs} from 'fs';
+const cards = document.getElementsByClassName("card");
+const reversedCards = [cards[-1], cards[-2], cards[-3]]
 
-const template = document.getElementById("template");
+const leftArrow = document.getElementById("left");
+const rightArrow = document.getElementById("right");
 
-fs.readdirSync("assets/images/test/").forEach((file) => {
-  console.log(file);
+rightArrow.addEventListener("click", () => {
+  let src = cards[cards.length - 1].children[0].src;
+
+  let srcTemp;
+
+  for (let element of cards) {
+    let img = element.children[0];
+    srcTemp = img.src;
+    img.src = src;
+    src = srcTemp;
+  }
 });
+
+leftArrow.addEventListener("click", () => {
+    let src = cards[0].children[0].src;
+
+    let srcTemp;
+
+    for (let element of reversedCards) {
+        let img = element.children[0];
+        srcTemp = img.src;
+        img.src = src;
+        src = srcTemp;
+      }
+  
+  });
