574b4b7b182a2122b307f26eba7a26a08e274086
NETyENeTA <gigasekond2@gmail.com>
2025-03-30 13:28:01 +0500

    F

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-const cards = [...document.getElementsByClassName("card")];
+///Extensions
 
-const leftArrow = document.getElementById("left");
-const rightArrow = document.getElementById("right");
+Array.prototype.classing = function (CLass) {
+  let array = [];
+  [...this].forEach((el) => {
+    array.push(new CLass(el));
+  });
+  return array;
+};
+
+/////
+///Classes
+
+class Image {
+  constructor(root) {
+    this.root = root;
+    console.log(this.srcGet().split('/'));
+    
+    this.Name(this.srcGet().split('/').at(-1))
+    this.Author("UnSigned")
+  }
+
+  srcGet() {
+    return this.root.children[0].src;
+  }
+
+  srcSet(value) {
+    this.root.children[0].src = value;
+  }
+
+  children(index) {
+    return this.root.children[index];
+  }
+
+  Author(name) {
+    this.root.children[1].children[0].textContent = name;
+  }
+
+  Name(name) {
+    this.root.children[1].children[1].textContent = name;
+  }
+}
+
+/////
+/// Functions
 
 function reversed(list) {
   let result = new Array();
@@ -11,28 +52,34 @@ function reversed(list) {
   return result;
 }
 
+/////
+/// Main
+
+const cards = [...document.getElementsByClassName("card")].classing(Image);
+
+const leftArrow = document.getElementById("left");
+const rightArrow = document.getElementById("right");
+
 leftArrow.addEventListener("click", () => {
-  let src = cards[cards.length - 1].children[0].src;
+  let src = cards[cards.length - 1].srcGet();
 
   let srcTemp;
 
   for (let element of cards) {
-    let img = element.children[0];
-    srcTemp = img.src;
-    img.src = src;
+    srcTemp = element.srcGet();
+    element.srcSet(src);
     src = srcTemp;
   }
 });
 
 rightArrow.addEventListener("click", () => {
-  let src = cards[0].children[0].src;
+  let src = cards[0].srcGet();
 
   let srcTemp;
 
   for (let element of reversed(cards)) {
-    let img = element.children[0];
-    srcTemp = img.src;
-    img.src = src;
+    srcTemp = element.srcGet();
+    element.srcSet(src);
     src = srcTemp;
   }
 });
